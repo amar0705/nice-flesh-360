e37f9521ce1306278761d9b650d1f9d027236192
faisalpinitod <faisalpinitod@gmail.com>
2023-05-04 14:17:44 +0530

    Different  hair styles data created

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,13 @@ const cors = require('cors')
 const {connection}=require('./config/db')
 const {stylist}=require('./routes/stylist.router')
 const {product}= require('./routes/product.router')
+const {styleRouter} =require('./routes/style.router')
 const app = express();
 app.use(express.json());
 app.use(cors())
 app.use("/stylist",stylist)
 app.use("/product",product)
+app.use("style",styleRouter)
 
 
 
