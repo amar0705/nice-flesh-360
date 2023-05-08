520e9c332c09539b319cd953a47fa704caf107a0
unknown <lluvporwall@gmail.com>
2023-05-08 14:34:05 +0530

    backed deploy on cyclic

--- Frontend/Scripts/product.js
+++ Frontend/Scripts/product.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:8080";
+const baseUrl = "https://elegant-hare-dungarees.cyclic.app";
 let BuyItems = JSON.parse(localStorage.getItem("items")) || [];
 ///api fetch
 mainFunction();
